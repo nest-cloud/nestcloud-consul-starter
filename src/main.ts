e9626e5293a1bb41e2115c51ab3fcae6de76ca10
miaowing <i@zfeng.net>
2019-02-28 15:27:42 +0800

    deregister consul service when exit app

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,20 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 async function bootstrap() {
     const boot = new Boot(__dirname);
     const app = await NestFactory.create(AppModule, { logger: new NestLogger() });
+
+    process.on('SIGINT', async () => {
+        setTimeout(() => process.exit(1), 5000);
+        await app.close();
+        process.exit(0);
+    });
+
+    //kill -15
+    process.on('SIGTERM', async () => {
+        setTimeout(() => process.exit(1), 5000);
+        await app.close();
+        process.exit(0);
+    });
+    
     await app.listen(boot.get('web.port', 8081));
 }
 
